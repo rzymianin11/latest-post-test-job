4285fb316aec82bbda817e6a77f54a816c7985bc
Lukasz Drozdz <lukasz.drozdz@pentacomp.pl>
2025-06-12 14:53:35 +0200

    small fix

--- wordpress/wp-content/plugins/blocks-brantt/build/latest-posts-brantt/index.asset.php
+++ wordpress/wp-content/plugins/blocks-brantt/build/latest-posts-brantt/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '963c74a9bc55f601e40f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'bc4c6016a7c9fac2953b');
